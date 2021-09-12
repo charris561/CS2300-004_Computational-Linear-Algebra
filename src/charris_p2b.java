14c3e52eae38a8853e4bee5ecb0bcfb3f76fbdb4
charris561 <charris4@uccs.edu>
2021-09-12 11:02:30 -0600

    finished p2b for PA01

--- src/charris_p2b.java
+++ src/charris_p2b.java
@@ -18,10 +18,93 @@ import java.util.Scanner;
 
 public class charris_p2b {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        double[][] testMatA = new double[1][3];
-        double[][] testMatB = new double[3][2];
+        //create constants for input file names
+        final String P1_MAT1 = "charris_p1_mat1.txt";
+        final String P1_MAT2 = "charris_p1_mat2.txt";
+        final String P1_MAT3 = "charris_p1_mat3.txt";
+        final String P1_MAT4 = "charris_p1_mat4.txt";
+        final String P1_MAT5 = "charris_p1_mat5.txt";
+
+        //create constants for output file names
+        //combos for mat 1
+        final String P2B_OUT11 = "charris_p2b_out11.txt";
+        final String P2B_OUT12 = "charris_p2b_out12.txt";
+        final String P2B_OUT13 = "charris_p2b_out13.txt";
+        final String P2B_OUT14 = "charris_p2b_out14.txt";
+        final String P2B_OUT15 = "charris_p2b_out15.txt";
+        //combos for mat2
+        final String P2B_OUT21 = "charris_p2b_out21.txt";
+        final String P2B_OUT22 = "charris_p2b_out22.txt";
+        final String P2B_OUT23 = "charris_p2b_out23.txt";
+        final String P2B_OUT24 = "charris_p2b_out24.txt";
+        final String P2B_OUT25 = "charris_p2b_out25.txt";
+        //combos for mat3
+        final String P2B_OUT31 = "charris_p2b_out31.txt";
+        final String P2B_OUT32 = "charris_p2b_out32.txt";
+        final String P2B_OUT33 = "charris_p2b_out33.txt";
+        final String P2B_OUT34 = "charris_p2b_out34.txt";
+        final String P2B_OUT35 = "charris_p2b_out35.txt";
+        //combos for mat4
+        final String P2B_OUT41 = "charris_p2b_out41.txt";
+        final String P2B_OUT42 = "charris_p2b_out42.txt";
+        final String P2B_OUT43 = "charris_p2b_out43.txt";
+        final String P2B_OUT44 = "charris_p2b_out44.txt";
+        final String P2B_OUT45 = "charris_p2b_out45.txt";
+        //combos for mat5
+        final String P2B_OUT51 = "charris_p2b_out51.txt";
+        final String P2B_OUT52 = "charris_p2b_out52.txt";
+        final String P2B_OUT53 = "charris_p2b_out53.txt";
+        final String P2B_OUT54 = "charris_p2b_out54.txt";
+        final String P2B_OUT55 = "charris_p2b_out55.txt";
+
+
+        //allocate memory for matrices from part 1
+        double[][] mat1 = mallocMat(P1_MAT1);
+        double[][] mat2 = mallocMat(P1_MAT2);
+        double[][] mat3 = mallocMat(P1_MAT3);
+        double[][] mat4 = mallocMat(P1_MAT4);
+        double[][] mat5 = mallocMat(P1_MAT5);
+
+        //fill matrices from files
+        readMatrix(P1_MAT1, mat1);
+        readMatrix(P1_MAT2, mat2);
+        readMatrix(P1_MAT3, mat3);
+        readMatrix(P1_MAT4, mat4);
+        readMatrix(P1_MAT5, mat5);
+
+        //calculate the addition of each file
+        //combos for mat1
+        calcMultiplication(mat1, mat1, P2B_OUT11);
+        calcMultiplication(mat1, mat2, P2B_OUT12);
+        calcMultiplication(mat1, mat3, P2B_OUT13);
+        calcMultiplication(mat1, mat4, P2B_OUT14);
+        calcMultiplication(mat1, mat5, P2B_OUT15);
+        //combos for mat2
+        calcMultiplication(mat2, mat1, P2B_OUT21);
+        calcMultiplication(mat2, mat2, P2B_OUT22);
+        calcMultiplication(mat2, mat3, P2B_OUT23);
+        calcMultiplication(mat2, mat4, P2B_OUT24);
+        calcMultiplication(mat2, mat5, P2B_OUT25);
+        //combos for mat3
+        calcMultiplication(mat3, mat1, P2B_OUT31);
+        calcMultiplication(mat3, mat2, P2B_OUT32);
+        calcMultiplication(mat3, mat3, P2B_OUT33);
+        calcMultiplication(mat3, mat4, P2B_OUT34);
+        calcMultiplication(mat3, mat5, P2B_OUT35);
+        //combos for mat4
+        calcMultiplication(mat4, mat1, P2B_OUT41);
+        calcMultiplication(mat4, mat2, P2B_OUT42);
+        calcMultiplication(mat4, mat3, P2B_OUT43);
+        calcMultiplication(mat4, mat4, P2B_OUT44);
+        calcMultiplication(mat4, mat5, P2B_OUT45);
+        //combos for mat5
+        calcMultiplication(mat5, mat1, P2B_OUT51);
+        calcMultiplication(mat5, mat2, P2B_OUT52);
+        calcMultiplication(mat5, mat3, P2B_OUT53);
+        calcMultiplication(mat5, mat4, P2B_OUT54);
+        calcMultiplication(mat5, mat5, P2B_OUT55);
 
     }//end main
 
@@ -36,9 +119,8 @@ public class charris_p2b {
         if (isPossible(matA, matB)){
 
             //allocate memory for matrix via [rows in matA][cols in matB]
-            double[][] mat12 = new double[matA.length][matB[0].length];
-            mat12 = multiplyMats(matA, matB);
-            printMatrix(mat12, fileToPrint);
+            double[][] matAB = multiplyMats(matA, matB);;
+            printMatrix(matAB, fileToPrint);
 
         }
 
@@ -51,19 +133,32 @@ public class charris_p2b {
     //addMatrices method takes two matrices and adds them or returns an error
     public static double[][] multiplyMats(double[][] matA, double[][] matB){
 
-        double[][] resultMat = new double[matA.length][matA[0].length];
+        //allocate memory for result mat
+        double[][] resultMat = new double[matA.length][matB[0].length];
 
-        //iterate through the rows, then the columns adding each index
-        for (int row = 0; row < matA.length; row++) {
+        double sum = 0; // used to store the sum or row * col
+        int i = 0; // used to track rows in matA
+        int j = 0; // used to track columns in matB
+        int k = 0; // used to track rows in matB
 
-            //iterate through columns
-            for (int col = 0; col < matA[0].length; col++) {
+        //multiplies each row by columns
+        for (i = 0; i < matA.length; i++){
 
-                //write code for multiplication
+            for (j = 0; j < matB[0].length; j++){
 
-            }//end col for
+                for (k = 0; k < matB.length; k++){
 
-        }//end row for
+                    sum += matA[i][k] * matB[k][j];
+
+                }//end matB row for loop
+
+                //assigns the sum value to result matrix then resets sum
+                resultMat[i][j] = sum;
+                sum = 0;
+
+            }//end matB col for loop
+
+        }//end matA row for loop
 
         return resultMat;
 
@@ -228,7 +323,7 @@ public class charris_p2b {
         PrintWriter outputFile = new PrintWriter(fileName);
 
         //print error to file
-        outputFile.println("Error: Addition is impossible");
+        outputFile.println("Error: Multiplication is impossible");
 
         //close file for printing
         outputFile.close();
